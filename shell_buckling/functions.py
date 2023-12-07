85524c8a3bda415afc8b8ed2b0d8fabcdd05be6c
LAPTOP-265UD690\Noahv <noahvdo@icloud.com>
2023-12-07 16:51:52 +0100

    removed calculate lambda and put it in function k

--- shell_buckling/functions.py
+++ shell_buckling/functions.py
@@ -11,15 +11,16 @@ def calculate_Q(p, E, R, t1):
     return p * (R / t1) ** 2 / E
 
 
-def calculate_lambda(L, R, t1, poisson):
+def calculate_k(L, R, t1, poisson):
     return math.sqrt(
         12 * L**4 * (1 - poisson**2) / (math.pi**4 * R**2 * t1**2)
-    )
-
-
-def calculate_k(_lambda, L, R, t1, poisson):
-    return _lambda + 12 * L**4 * (1 - poisson**2) / (
-        math.pi**4 * R**2 * t1**2 * _lambda
+    ) + 12 * L**4 * (1 - poisson**2) / (
+        math.pi**4
+        * R**2
+        * t1**2
+        * math.sqrt(
+            12 * L**4 * (1 - poisson**2) / (math.pi**4 * R**2 * t1**2)
+        )
     )
 
 
