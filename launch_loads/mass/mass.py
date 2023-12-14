f1d9f30553dc52b01943e158a490473a2f33ea26
Noah <noahvdo@icloud.com>
2023-12-14 15:08:32 +0100

    created function that calculates R numerically

--- launch_loads/mass/mass.py
+++ launch_loads/mass/mass.py
@@ -1,4 +1,5 @@
 import math
+from decimal import Decimal
 
 
 def get_volume(R, L, t1, t2):
@@ -9,6 +10,16 @@ def get_volume(R, L, t1, t2):
 
 def get_fuel_volume(R, L, t1, t2):
     return round(((R) ** 2) * math.pi * (L - 2 * R) + (4 / 3) * math.pi * ((R) ** 3), 5)
+
+
+def find_r_for_volume(volume, L, t, R):
+    while not found_value:
+        fuel_volume = get_fuel_volume(R, L, t, t)
+        if fuel_volume >= volume:
+            found_value = True
+        else:
+            R += 0.00001
+    return R, fuel_volume
 
 
 # Calculation of the mass of the vessel
